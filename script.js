fe88c3f2da4b3d2270684286ecca0b433c86d4e0
elmakhtarlaarif <mokhtarlaarif54@gmail.com>
2025-10-30 14:18:07 +0100

    code html css et js de premier section

--- script.js
+++ script.js
@@ -30,73 +30,13 @@ reserveButtons.forEach((btn) => {
       places: eventCard.querySelectorAll("p")[2].textContent,
       prix: eventCard.querySelectorAll("p")[3].textContent
     };
-
     reservation.evenement = eventData;
     console.log("Événement choisi :", reservation.evenement);
-    const data = localStorage.setItem("reserve", JSON.stringify(reservation.evenement));
-    console.log(data.titre);
     choix.style.display = "none";
     billet.style.display = "block";
     InformationsParticipants.style.display = "none";
 
     firstCircle.style.backgroundColor = "#078E51";
     firstLine.style.backgroundColor = "#078E51";
-
-    updateBilletSection();
-  });
-});
-
-function updateBilletSection() {
-  const billetContent = billet.querySelector(".billets-content");
-  const e = data;
-
-  billetContent.innerHTML = `
-    <h3>${e.titre}</h3>
-    <p>${e.date}</p>
-    <p>${e.lieu}</p>
-    <p>${e.places}</p>
-    <p>${e.prix}</p>
-  `;
-}
-
-let ticketCount = 1;
-const ticketDisplay = document.querySelector(".nombre div:nth-child(2)");
-const minusBtn = document.querySelector(".nombre div:nth-child(1)");
-const plusBtn = document.querySelector(".nombre div:nth-child(3)");
-
-plusBtn.addEventListener("click", () => {
-  ticketCount++;
-  ticketDisplay.textContent = ticketCount;
-  reservation.billets = ticketCount;
-});
-
-minusBtn.addEventListener("click", () => {
-  if (ticketCount > 1) {
-    ticketCount--;
-    ticketDisplay.textContent = ticketCount;
-    reservation.billets = ticketCount;
-  }
-});
-
-function nextPage(index) {
-  if (index === 1) {
-    billet.style.display = "none";
-    InformationsParticipants.style.display = "block";
-
-    secondCircle.style.backgroundColor = "#078E51";
-    secondLine.style.backgroundColor = "#078E51";
-
-    buildParticipantForms();
-  }
-}
-prevButtons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    if (InformationsParticipants.style.display === "block") {
-      InformationsParticipants.style.display = "none";
-      billet.style.display = "block";
-    } else if (billet.style.display === "block") {
-      billet.style.display = "none";
-      choix.style.display = "block";
-    }
   });
 });
